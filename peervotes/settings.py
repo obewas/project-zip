8280211d5dcd0145ca536f1150d2a24083687f9a
obewas <obewas1202@gmail.com>
2021-07-26 10:00:29 +0300

    changes static files dirs

--- peervotes/settings.py
+++ peervotes/settings.py
@@ -169,7 +169,9 @@ CLOUDINARY_STORAGE = {
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 
 MEDIA_URL = '/media/'
